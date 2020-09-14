f66a8ad68dc38b91f99a3143059a56dabcf9edd5
XivC <57796626+XivC@users.noreply.github.com>
2020-09-14 09:34:28 +0300

    cm2

--- Lab1.java
+++ Lab1.java
@@ -26,7 +26,7 @@ public class Lab1 {
 		}
 
 		for (int i = 0; i < 9; i++){
-			for (int j = 0; j < 9; j++){
+			for (int j = 0; j < 20; j++){
 
 				if (d[i] == 14) b[i][j] = (Math.asin(Math.PI/4 * Math.pow(Math.E, -Math.abs(x[j]))));
 				else if (isInArray(d[i], new short[] {6, 8, 10, 18} )) b[i][j] = (Math.tan(Math.PI * Math.pow(1/3 + Math.pow(x[j], 1/3),3)));
