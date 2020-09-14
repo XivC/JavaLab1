f092aa0ae1f8200fb46424e8746f3386ddb28c27
XivC <57796626+XivC@users.noreply.github.com>
2020-09-14 10:22:40 +0300

    cm3

--- Lab1.java
+++ Lab1.java
@@ -15,6 +15,7 @@ public class Lab1 {
 
 	public static void main(String args[]){
 
+
 		short[] d = {4, 6, 8, 10, 12, 14, 16, 18, 20};
 		double[] x = new double[20];
 		double left_side = -2; //Левая граница случайного значания в массиве d
@@ -29,8 +30,8 @@ public class Lab1 {
 			for (int j = 0; j < 20; j++){
 
 				if (d[i] == 14) b[i][j] = (Math.asin(Math.PI/4 * Math.pow(Math.E, -Math.abs(x[j]))));
-				else if (isInArray(d[i], new short[] {6, 8, 10, 18} )) b[i][j] = (Math.tan(Math.PI * Math.pow(1/3 + Math.pow(x[j], 1/3),3)));
-				else b[i][j] = (Math.cos(Math.pow(Math.pow(Math.cos(x[j]),1/3), (Math.atan(Math.pow(Math.E, -Math.abs(x[j]))) - 1)/(Math.atan(Math.cos(x[j]))))));
+				else if (isInArray(d[i], new short[] {6, 8, 10, 18} )) b[i][j] = (Math.tan(Math.PI * Math.pow(1/3 + Math.pow(x[j], 1/3D),3)));
+				else b[i][j] = (Math.cos(Math.pow(Math.pow(Math.cos(x[j]),1/3D), (Math.atan(Math.pow(Math.E, -Math.abs(x[j]))) - 1)/(Math.atan(Math.cos(x[j]))))));
 				System.out.printf("%.5f", b[i][j]);
 				System.out.print(" | ");
 			}
